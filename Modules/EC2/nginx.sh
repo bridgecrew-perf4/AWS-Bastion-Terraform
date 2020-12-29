77b59dc25783956fd62419475189f4b2020fa28d
marko0486 <marko1986@gmail.com>
2020-12-28 22:41:43 -0600

    Fixed EC2.tf and rsync recursive

--- Modules/EC2/nginx.sh
+++ Modules/EC2/nginx.sh
@@ -9,7 +9,7 @@ sudo http_proxy=http://10.0.10.10:8888 apt install nginx -y
 sudo rm -rf /var/www/html/*.*
 ENDSSH
 
-rsync -avz -e "ssh -i /tmp/aws-key.pem" --rsync-path='sudo rsync' /tmp/index/*.* ubuntu@10.0.20.20:/var/www/html/
+rsync --recursive -avz -e "ssh -i /tmp/aws-key.pem" --rsync-path='sudo rsync' /tmp/index/ ubuntu@10.0.20.20:/var/www/html/
 
 ssh -i /tmp/aws-key.pem -o  'StrictHostKeyChecking no' ubuntu@10.0.20.20 'sudo service nginx restart'
 echo "***************************************************************************************************************"
